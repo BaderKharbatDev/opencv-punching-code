4cc644a011df11139860b5d2772ab5bc00b643d7
Bader Kharbat <bgkharba@ncsu.edu>
2021-05-09 12:25:59 -0400

    about to modify arduino code:

---
+++ arduino_servo_code/arduino_servo_code.ino
@@ -0,0 +1,52 @@
+  #include <Servo.h>
+
+//hardware vars
+Servo tilt_servo;
+Servo base_servo;
+int base_servo_pin = 9;
+int tilt_servo_pin = 11;
+int relay_pin = 6;
+
+//software vars
+int base_servo_value = 90;
+int tilt_servo_value = 90;
+bool rotationChange = false;
+
+void setup() {
+  pinMode(relay_pin, OUTPUT);
+  Serial.begin(9600);
+  Serial.setTimeout(10000);
+  base_servo.attach(base_servo_pin);
+  base_servo.write(base_servo_value);
+  tilt_servo.attach(tilt_servo_pin);
+  tilt_servo.write(tilt_servo_value);
+}
+
+void loop() {
+  if(rotationChange) {
+    base_servo.write(base_servo_value);
+    tilt_servo.write(tilt_servo_value);
+    rotationChange = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    String command = Serial.readStringUntil('|');
+    if(command=="a"){
+      String inByteA = Serial.readStringUntil('/'); // read data until newline
+      String inByteB = Serial.readStringUntil('\n'); // read data until newline
+      base_servo_value = inByteA.toInt();
+      tilt_servo_value = inByteB.toInt();
+      rotationChange = true;
+    }else if(command=="b"){
+      String inByteB = Serial.readStringUntil('\n'); // read data until newline 
+      if(inByteB=="a"){
+         digitalWrite(relay_pin, HIGH);
+      } else if(inByteB=="b"){
+         digitalWrite(relay_pin, LOW);
+      }
+    } 
+  }
+}
--- src/opencv-face-test.py
+++ src/opencv-face-test.py
@@ -3,11 +3,14 @@ import threading
 import time
 import atexit
 import helper_functions as help
+import serial
 
 # important fields
-face_classifier = cv2.CascadeClassifier('resources/haarcascade_frontalface_default.xml')
-eye_classifier = cv2.CascadeClassifier('resources/haarcascade_eye.xml')
-capture = cv2.VideoCapture(1)
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
+# lock = False
 
 # global face or eye array
 faces = []
@@ -22,8 +25,8 @@ def captureUserTarget():
 
         # checks for face and then eyes if unavailable
         faces = face_classifier.detectMultiScale(gray, 1.1, 4)
-        if(len(faces) == 0):
-            faces = eye_classifier.detectMultiScale(gray, 1.1, 4)
+        # if(len(faces) == 0):
+        #     faces = eye_classifier.detectMultiScale(gray, 1.1, 4)
 
         # draws around users face
         for (x, y, w, h) in faces:
@@ -34,8 +37,6 @@ def captureUserTarget():
         cv2.imshow('Video', frame)
         cv2.waitKey(20)
 
-
-
 def trackUser(img):
     if(capture.isOpened() and len(faces) > 0):
         print("")
@@ -43,9 +44,8 @@ def trackUser(img):
         capture_width = capture.get(3)
 
         f = faces[0]
-        x = (f[0]+f[2])/capture_width
-        y = (f[1]+f[3])/capture_height
-        # help.printf("X: %0.2f Y:%0.2f",x, y)
+        x = (f[0]+f[2]/2)/capture_width
+        y = (f[1]+f[3]/2)/capture_height
         text = "X: {:0.0f}% Y: {:0.0f}%".format(x*100, y*100)
         cv2.putText(img, text, 
             (10,int(capture_height)-25), 
@@ -53,8 +53,29 @@ def trackUser(img):
             1,
             (255,255,255),
             2)
-
+        # if not lock:
+        setServos(x,y)
     return img
+
+def setServos(x,y):
+    inner_circle_radius = 40
+    inner_circle_radius_y = 40
+
+    adjusted_x = (1-x)*180
+    adjusted_y = (1-y)*180
+    if(adjusted_x < 90-inner_circle_radius):
+        adjusted_x = 90 - inner_circle_radius
+    elif(adjusted_x > 90+inner_circle_radius):
+        adjusted_x = 90 + inner_circle_radius
+
+    if(adjusted_y < 90-inner_circle_radius_y):
+        adjusted_y = 90 - inner_circle_radius_y
+    elif(adjusted_y > 90+inner_circle_radius_y):
+        adjusted_y = 90 + inner_circle_radius_y
+
+    rs = "a|"+str(adjusted_x)+"/"+str(adjusted_y)+"\n"
+    print(rs)
+    arduino.write(str.encode(rs))
 
 #Detection Thred
 capture_thread = threading.Thread(target=captureUserTarget, args=())
@@ -70,10 +91,18 @@ atexit.register(exit_handler)
 # main thread
 capture_thread.start() # starts face tracking in the background
 # gets menu input
-
+frequency = 5 #difficulty [1 - 10]
+delay_time = frequency*-1+11
 # starts fsm
 while True:
+    # lock = True
+    rs = "b|a\n";
+    arduino.write(str.encode(rs))
     time.sleep(0.5)
+    rs = "b|b\n";
+    arduino.write(str.encode(rs))
+    # lock = False
+    time.sleep(delay_time/10)
     
 
     
